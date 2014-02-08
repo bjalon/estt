469deea2a191546b3d9490d5ffdd91205fd6bc8c
Benjamin JALON <bjalon@nuxeo.com>
2014-02-08 11:07:54 +0100

    Start work on rechercher behavior

--- nuxeo-est-we-front/src/main/yo/app/scripts/controllers/recherche.js
+++ nuxeo-est-we-front/src/main/yo/app/scripts/controllers/recherche.js
@@ -3,6 +3,15 @@
 angular.module('nuxeoBibliothequeFrontApp')
     .controller('RechercheCtrl', function ($scope, $http, $location) {
 
-
-    }); 
+    $scope.rechercher = function() {
+      var postData = { text : 'long blob of text' } ;
+      var config = {params : { id: ' 5 ' } } ;
+      
+	  Shttp.post ('/nuxeo/site/front/search/livre', postData, config).success(function(data, status, headers, config) {
+	    // Do something successful
+      } ).error (function (data, status, headers, config) {
+		// Handle the error
+	  })
+    }
+}); 
 
